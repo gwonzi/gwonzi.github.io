be4413e3d82cdb0c3f43c91e8711211475049f0b
Chris Hyman <chris@wugs.me>
2018-12-24 11:17:10 -0800

    Attempt to fix #4

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,7 +51,7 @@
 		// Button.
 			$(
 				'<div id="navButton">' +
-					'<a href="#navPanel" class="toggle" display="inline-block"><span class="fas fa-bars"></span></a>' +
+					'<a href="#navPanel" class="toggle"><span class="fas fa-bars navbut"></span></a>' +
 				'</div>'
 			)
 				.appendTo($body);
