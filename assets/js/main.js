2e805bfc9843265536dc93e2aec756f12bf6ec75
Chris Hyman <chris@wugs.me>
2018-12-24 11:21:16 -0800

    Update main.js
    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,7 +51,7 @@
 		// Button.
 			$(
 				'<div id="navButton">' +
-					'<a href="#navPanel" class="toggle"><span class="fas fa-bars navbut"></span></a>' +
+					'<a href="#navPanel" class="toggle"></a>' +
 				'</div>'
 			)
 				.appendTo($body);
