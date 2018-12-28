d8b10c4870f377c7a1b96ac10a30793179bc7917
Chris Hyman <chris@wugs.me>
2018-12-28 14:54:04 -0800

    jquery table standings

---
+++ assets/js/dom/s1.js
@@ -0,0 +1,20 @@
+$("#s1").click(function() {
+  var tablehtml = "\
+  <table> \
+    <tr> \
+      <th>Game</th> \
+      <th>1st</th> \
+      <th>2nd</th> \
+      <th>3rd</th> \
+      <th>4th</th> \
+    </tr> \
+    <tr> \
+      <th>1</th> \
+      <td>Chris</td> \
+      <td>Ryan</td> \
+      <td>Eduardo</td> \
+      <td>Corey</td> \
+    </tr> \
+  </table>";
+  $("#rtable").html(tablehtml);
+});
