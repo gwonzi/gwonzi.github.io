1e40e667324df099f1d32e6385cd6c41caef3001
Chris Hyman <chris@wugs.me>
2018-12-28 20:27:34 -0800

    Update standings.js
    no totals

--- assets/js/dom/standings.js
+++ assets/js/dom/standings.js
@@ -23,9 +23,9 @@ $("#s1").click(function() {
   <table class="ptab"> \
     <tr> \
       <th>Player</th> \
-      <th colspan="4">Rank Points</th> \
+      <th colspan="4">Record</th> \
+      <th>Rank Points</th> \
       <th>Bonus Points</th> \
-      <th>Total</th> \
       <th>Season Rank</th> \
     </tr> \
     <tr> \
@@ -44,63 +44,9 @@ $("#s1").click(function() {
       <td>0</td> \
       <td>0</td> \
       <td>0</td> \
+      <td>4</td> \
       <td>' + chris.s1g1.bp_total() + '</td> \
-      <td>7</td> \
       <td>1st</td> \
-    </tr> \
-  </table>';
-
-  $("#rtable").html(rtable);
-  $("#ptable").html(ptable);
-});
-
-$("#s2").click(function() {
-  var rtable = '\
-  <table class="rtab"> \
-    <tr> \
-      <th>Game</th> \
-      <th>1st</th> \
-      <th>2nd</th> \
-      <th>3rd</th> \
-      <th>4th</th> \
-    </tr> \
-    <tr> \
-      <th>1</th> \
-      <td>Another</td> \
-      <td>Sample</td> \
-      <td>Game</td> \
-      <td>Table</td> \
-    </tr> \
-  </table>';
-
-  var ptable = '\
-  <table class="ptab"> \
-    <tr> \
-      <th>Player</th> \
-      <th colspan="4">Rank Points</th> \
-      <th>Bonus Points</th> \
-      <th>Total</th> \
-      <th>Season Rank</th> \
-    </tr> \
-    <tr> \
-      <td class="emptycell"></td> \
-      <th class="rp-small">1st</th> \
-      <th class="rp-small">2nd</th> \
-      <th class="rp-small">3rd</th> \
-      <th class="rp-small">4th</th> \
-      <td class="emptycell"></td> \
-      <td class="emptycell"></td> \
-      <td class="emptycell"></td> \
-    </tr> \
-    <tr> \
-      <th>Chris</th> \
-      <td>0</td> \
-      <td>0</td> \
-      <td>0</td> \
-      <td>0</td> \
-      <td>0</td> \
-      <td>0</td> \
-      <td>0th</td> \
     </tr> \
   </table>';
 
